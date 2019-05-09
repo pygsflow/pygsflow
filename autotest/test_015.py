7e7c86d924f19f7848798de92a00208c84d01b73
Joshua Larsen <jlarsen@usgs.gov>
2019-05-09 12:10:07 -0700

    Added rasters.py and Rasters() which allows for the import of raster data and sampling to hrus based on the PrmsDiscretization object!
    * this module uses rasterio and gdal to accomplish raster processing

---
+++ autotest/test_015.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from gsflow.utils import Rasters
+from gsflow.output import PrmsDiscretization
+
+
+def test_raster_functions():
+
+    # load the PrmsDiscrtization object
+    ws = r"../examples/data/sagehen/shapefiles"
+    shp = "hru_params.shp"
+    dis = PrmsDiscretization.load_from_shapefile(os.path.join(ws, shp))
+
+    # load a DEM raster of the area
+    path = r'../examples/data/geospatial'
+    name = "dem.img"
+    x = Rasters.load(os.path.join(path, name))
+    x.set_raster_band(1)
+    dem = x.sample_discretization(dis)
+
+    if not isinstance(dem, np.ndarray):
+        raise AssertionError()
+
+    if len(dem) != len(dis.x_hru_centers):
+        raise AssertionError
+
+    xp = x.xpoints
+    yp = x.ypoints
+    xyp = x.xypoints
+
+    if xp is None:
+        raise AssertionError()
+    if yp is None:
+        raise AssertionError()
+    if xyp is None:
+        raise AssertionError()
+
+
+    array = x.band_array
+    if not isinstance(array, np.ndarray):
+        raise AssertionError()
+
+if __name__ == "__main__":
+    test_raster_functions()
--- gsflow/utils/__init__.py
+++ gsflow/utils/__init__.py
@@ -1,5 +1,6 @@
 from .io import find_parameter, get_file_abs, _get_relative_path
 from .constants import GsConstant
+from .rasters import Rasters
 from . import mfreadnam
 from . import io
 from . import visu
