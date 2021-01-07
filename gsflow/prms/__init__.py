22102cc8b62d57fb6109fb23d8a6bdd18c1c405c
aymanalz <aalzraiee@usgs.gov>
2021-01-07 12:09:34 -0800

    Dynamic Parameters are added

--- gsflow/prms/__init__.py
+++ gsflow/prms/__init__.py
@@ -3,3 +3,4 @@ from .prms_parameter import PrmsParameters
 from .prms_parameter import ParameterRecord
 from .prms_data import PrmsData
 from .prms_help import Helper
+from .prms_dynamic_params import Dyn_param
