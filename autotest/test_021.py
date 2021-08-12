5fec4ba47931516110c95f1bce9d32f8bc6be339
Joshua Larsen <jlarsen@usgs.gov>
2021-08-11 18:39:50 -0700

    Feat(CRT): cascade routing tool driver module
    * added base CRT class to crt.py
    * added CRT linux (ubuntu) and windows binaries to bin folder
    * added test_021.py for autotesting

---
+++ autotest/test_021.py
@@ -0,0 +1,27 @@
+import os
+
+
+ws = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_import_crt():
+    from gsflow.crt import CRT
+
+
+def test_read_write_crt():
+    from gsflow.crt import CRT
+    exe_name = os.path.join(ws, "..", "bin", "CRT_1.3.1")
+    model_ws = os.path.join(ws, "..", "examples", "data", "crt")
+    out_ws = os.path.join(ws, "temp")
+
+    crt = CRT.load(model_ws)
+    crt.write_input(out_ws)
+    success, buff = crt.run_model(exe_name)
+
+    if not success:
+        raise AssertionError("CRT run failed")
+
+
+if __name__ == "__main__":
+    test_import_crt()
+    test_read_write_crt()
