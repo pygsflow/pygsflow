30757a58b362129bc6ac30b0054a07a65a0ae86b
Quiroz <jonathanqv1@gmail.com>
2022-04-21 00:47:30 -0600

    Added a functionality to load prms default parameters

--- gsflow/builder/__init__.py
+++ gsflow/builder/__init__.py
@@ -8,5 +8,6 @@ from .fishnet import GenerateFishnet
 from .modflow_builder import ModflowBuilder
 from .controlfile_builder import ControlFileBuilder
 from .prms_builder import PrmsBuilder
+from .paramfile_builder import ParameterFileBuilder
 from .flow_accumulation import FlowAccumulation
 from . import builder_utils
