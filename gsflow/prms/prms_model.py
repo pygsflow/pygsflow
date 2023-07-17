b87bff0014a3ea87835bf4e23aea6a09163b91c2
Joshua Larsen <jlarsen@usgs.gov>
2023-07-17 09:10:36 -0700

    fix broken PrmsData file load with model_ws issue

--- gsflow/prms/prms_model.py
+++ gsflow/prms/prms_model.py
@@ -100,7 +100,10 @@ class PrmsModel(object):
             ]
             if "data_file" in control.record_names:
                 data_file = control.get_values("data_file")[0]
-                data_file = gsflow_io.get_file_abs(control_file, data_file)
+                data_file = gsflow_io.get_file_abs(
+                    model_ws=model_ws,
+                    fn=data_file
+                )
             else:
                 data_file = None
 
