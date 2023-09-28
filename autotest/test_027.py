bc22dc67f994c79e2370aa5bd76835f12335ec4d
@pmccarthy <pmccarthy@usgs.gov>
2023-09-28 11:49:53 -0700

    test(develop): test_upslope_streams unittest
    fixed model_ws path causing CI to fail

--- autotest/test_027.py
+++ autotest/test_027.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 ws = os.path.abspath(os.path.dirname(__file__))
-model_ws = os.path.join("..", "examples", "data", "sagehen", "50m_tutorials")
+model_ws = os.path.join(ws, "..", "examples", "data", "sagehen", "50m_tutorials")
 control = os.path.join(model_ws, "sagehen_50m_initial.control")
 
 
@@ -19,7 +19,6 @@ def test_upslope_streams_with_hru():
     gsf = gsflow.GsflowModel.load_from_file(control)
     params = gsf.prms.parameters
     strms = params.upslope_streams(stream_hru=10787)
-    print(strms)
     if not isinstance(strms, dict):
         raise AssertionError("Upslope_streams returned incorrect type")
     if not len(strms) == 41:
